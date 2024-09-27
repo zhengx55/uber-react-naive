48a63d89684fb488d751065a53b09fb121c806d3
zhengx55 <williamzhxin@gmail.com>
2024-09-28 01:20:57 +0800

    Refactor babel.config.js and add nativewind plugin

---
+++ app/(root)/_layout.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const RootLayout = () => {
+  return (
+    <View>
+      <Text>RootLayout</Text>
+    </View>
+  );
+};
+
+export default RootLayout;
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -15,7 +15,7 @@ const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
 
 if (!publishableKey) {
   throw new Error(
-    "Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env"
+    "Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env",
   );
 }
 
